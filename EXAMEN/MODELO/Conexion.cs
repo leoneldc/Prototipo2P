6b39a3de647b9935ca46819471c1a4db8df9580b
leoneldc <leonelchavez2013@hotmail.com>
2022-09-27 11:22:39 -0600

    creación del conetenedor mdi

---
+++ EXAMEN/MODELO/Conexion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Odbc;
+
+namespace MODELO
+{
+    class Conexion
+    {
+        public OdbcConnection conexion()
+        {
+
+            OdbcConnection conn = new OdbcConnection("Dsn=2P");
+            try
+            {
+                conn.Open();
+            }
+            catch (OdbcException)
+            {
+                Console.WriteLine("Error al conectar");
+            }
+            return conn;
+        }
+
+
+        public void desconexion(OdbcConnection conn)
+        {
+            try
+            {
+                conn.Close();
+            }
+            catch (OdbcException)
+            {
+                Console.WriteLine("Error al desconectar");
+            }
+        }
+    }
+}
